11b99092072dd16774b8d431fa596e693fdd52b6
Séverin Robert <sevrobe21@gmail.com>
2021-07-08 18:23:09 +0200

    unitest class Personnage et Labyrinthe + try except taille lab

---
+++ libs/labytest.py
@@ -0,0 +1,75 @@
+import unittest
+from libs.personnage import Personnage
+from libs.labyrinthe import Labyrinthe
+
+lb = ["##########",
+      "#        #",
+      "# #####  #",
+      "# #   ####",
+      "# # #     ",
+      "# # ######",
+      "# #      #",
+      "# ###### #",
+      "#        #",
+      "##########"]
+depart = [2, 8]
+arrivee = [4, 9]
+lab1 = Labyrinthe(lb, len(lb), depart, arrivee)
+lab_visible1 = Labyrinthe.creation_lab.__get__(lab1)
+
+lb = ["#####",
+      "#   #",
+      "# ###",
+      "#    ",
+      "#####"]
+depart = [1, 3]
+arrivee = [3, 4]
+lab2 = Labyrinthe(lb, len(lb), depart, arrivee)
+lab_visible2 = Labyrinthe.creation_lab.__get__(lab2)
+
+class LabyTestCase(unittest.TestCase):
+
+    def test_type_renvoye(self):
+        """Vérification du type renvoyé"""
+        self.assertIsInstance(lab_visible1, list)
+        self.assertIsInstance(lab_visible1[1], str)
+        self.assertIsInstance(lab_visible2, list)
+        self.assertIsInstance(lab_visible2[1], str)
+
+    def test_contenu(self):
+
+        self.assertEqual(lab_visible2, ["#####",
+                                        "#---#",
+                                        "#---#",
+                                        "#----",
+                                        "#####"])
+
+class PersoTestCase(unittest.TestCase):
+
+    def test_deplacement_correcte(self):
+        """Déplacements possible"""
+        self.assertEqual(Personnage().deplacement(lab2, "d", [1, 2], lab_visible2), [1, 3])
+        self.assertEqual(Personnage().deplacement(lab2, "q", [1, 2], lab_visible2), [1, 1])
+        self.assertEqual(Personnage().deplacement(lab2, "z", [2, 1], lab_visible2), [1, 1])
+        self.assertEqual(Personnage().deplacement(lab2, "s", [2, 1], lab_visible2), [3, 1])
+
+    def test_deplacement_vers_mur(self):
+        """Déplacements sur un mur"""
+        self.assertEqual(Personnage().deplacement(lab2, "d", [2, 1], lab_visible2), [1, 3])
+        self.assertEqual(Personnage().deplacement(lab2, "q", [1, 1], lab_visible2), [1, 3])
+        self.assertEqual(Personnage().deplacement(lab2, "z", [1, 2], lab_visible2), [1, 3])
+        self.assertEqual(Personnage().deplacement(lab2, "s", [3, 3], lab_visible2), [1, 3])
+
+    def test_mauvais_input(self):
+        """Mauvais input"""
+        self.assertEqual(Personnage().deplacement(lab2, "r", [2, 1], lab_visible2), [2, 1])
+        self.assertEqual(Personnage().deplacement(lab2, "2", [1, 1], lab_visible2), [1, 1])
+        self.assertEqual(Personnage().deplacement(lab2, "@", [1, 2], lab_visible2), [1, 2])
+        self.assertEqual(Personnage().deplacement(lab2, "", [3, 3], lab_visible2), [3, 3])
+
+
+if __name__ == '__main__':
+    # ATTENTION : Pour exécuter ce test dans un notebook, il faut utiliser un appel à unittest.main() modifié.
+    # Dans PyCharm, vous pouvez utiliser la version normale sans paramètre, cfr ci-dessous.
+    # unittest.main()
+    unittest.main()
--- main.py
+++ main.py
@@ -1,13 +1,8 @@
 from os import system
-
 from libs.personnage import Personnage
-
 from libs.console import jeu_console
-
 from libs.gui import jeu_gui
-
 from libs.rang import rng_lab
-
 from libs.labyrinthe import Labyrinthe
 
 # test
@@ -15,12 +10,23 @@ from libs.labyrinthe import Labyrinthe
 joueur = Personnage()
 
 print("Bonjour bienvenue dans le labyrinthe !\nVotre but est d'atteindre la sortie en vous dirigeant à l'aide"
-          " de q,d,z,s (gauche,droite,haut,bas)\nMais attention aux murs qui ne seront visible que lorsque vous "
-          "vous y cognerez!")
+      " de q,d,z,s (gauche,droite,haut,bas)\nMais attention aux murs qui ne seront visible que lorsque vous "
+      "vous y cognerez!")
 type_lab = input('voulez-vous jouer des labyrinthes random ? o/n')
 if type_lab == "o":
-    taille = int(input("quelle taille ?21/31/41/51"))
-    lab1 = Labyrinthe([], taille, [], [])
+    taille = 0
+    while taille < 7 and taille % 2 == 0:
+        try:
+            taille = int(input("Choisissez une taille impaire au dessus de 7 : "))
+            break
+        except ValueError:
+            print("Ceci n'est pas un nombre")
+        except Exception as e:
+            # Just print(e) is cleaner and more likely what you want,
+            # but if you insist on printing message specifically whenever possible...
+            print(f"Une erreur s'est produite : {e} {e.message}")
+
+    lab1 = Labyrinthe([], int(taille), [], [])
     rng_lab(lab1)
     lab1.affichage(lab1.lab)
 else:
